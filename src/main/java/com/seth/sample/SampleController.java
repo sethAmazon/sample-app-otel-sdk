a7d61724366fa19debe2125cd2067485049e06ee
Seth Levine <sethlevi@amazon.com>
2023-11-28 16:48:55 -0500

    Move Counter Out Of Function

--- src/main/java/com/seth/sample/SampleController.java
+++ src/main/java/com/seth/sample/SampleController.java
@@ -15,25 +15,24 @@ public class SampleController {
     @Autowired
     OpenTelemetry openTelemetry;
 
+    Meter meter = openTelemetry.meterBuilder("instrumentation-library-name")
+            .setInstrumentationVersion("1.0.0")
+            .build();
+
+    // Build counter e.g. LongCounter
+    LongCounter counter = meter
+            .counterBuilder("processed_jobs")
+            .setDescription("Processed jobs")
+            .setUnit("1")
+            .build();
+
+    // It is recommended that the API user keep a reference to Attributes they will record against
+    Attributes attributes = Attributes.of(AttributeKey.stringKey("Key"), "SomeWork");
+
     @GetMapping("/")
     public String helloWorld() throws InterruptedException {
-        Meter meter = openTelemetry.meterBuilder("instrumentation-library-name")
-                .setInstrumentationVersion("1.0.0")
-                .build();
-
-        // Build counter e.g. LongCounter
-        LongCounter counter = meter
-                .counterBuilder("processed_jobs")
-                .setDescription("Processed jobs")
-                .setUnit("1")
-                .build();
-
-        // It is recommended that the API user keep a reference to Attributes they will record against
-        Attributes attributes = Attributes.of(AttributeKey.stringKey("Key"), "SomeWork");
-
         // Record data
         counter.add(123, attributes);
-        Thread.sleep(5000);
         return "Hello World";
     }
 }
